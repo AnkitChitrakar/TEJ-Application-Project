307399ccad47ab8300b2f31cfb8337d7084ba57e
ANkit Chitrakar <56572874+AnkitChitrakar@users.noreply.github.com>
2022-04-12 18:36:06 +0545

    Add files via upload

---
+++ TEJ/js/scripts.js
@@ -0,0 +1,22 @@
+window.alert("Hello TEJ!");
+
+
+function alertbox() {
+    alert("Hello There");
+}
+
+
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+function decrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('number').value = value;
+}
